03bc1f040c98fcf3a21fa33915d1fa0814ecf067
Sneko <Sneko@users.noreply.github.com>
2014-10-18 01:21:15 +0200

    Create login_success.js

---
+++ web/login_success.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Facebook Alternative Plugin - By Thomas Ramé</title>
+		<style>
+			html, body {
+				width: 100%;
+				height: 100%;
+				margin: 0;
+				padding: 0;
+				background-color: #3d5e96;
+				overflow:hidden;
+			}
+		</style>
+	</head>
+	<body>
+		<p>Login Success!</p>
+	</body>
+</html>
